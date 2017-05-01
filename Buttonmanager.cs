48df6c778ad3da271c0737b86c1ac1cd71c49185
Saran N Subramaniyan <sarannsworld@gmail.com>
2017-05-01 16:54:36 +0530

    Comments added
    Script has to be attached in all buttons and pass respective value that corresponds to its value in the Build settings

--- Buttonmanager.cs
+++ Buttonmanager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+// Attach the script to the  Canvas-Panel-Button which gives its corresponding functionality.
+
+using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
 namespace M{
@@ -12,7 +14,7 @@ namespace M{
 		public void PlayButton(string Play)
 		{
 
-			SceneManager.LoadScene(1);
+			SceneManager.LoadScene(1);  // Check the Build settings and pass/change the argument value for Load scene
 		}
 
 		public void ExitButton()
